98be249b1994f61fcaaffebdb557ae534c2478e6
nicolasdca <nicolasdavid193@gmail.com>
2023-10-25 13:42:10 -0300

    Lógica JS 2 Alura - Aula 2

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-function verificarChute() {}
+let numeroAleatorio = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+  let chute = document.querySelector('input').value;
+  if (chute == numeroAleatorio) {
+    exibirTextoNaTela('p', 'Você acertou!');
+  } else if (chute < numeroAleatorio) {
+    exibirTextoNaTela('p', `O número é maior que ${chute}`);
+  } else {
+    exibirTextoNaTela('p', `O número é menor que ${chute}`);
+  }
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
