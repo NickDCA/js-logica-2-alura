8c7caa92174238891f8ea102f33229a314e4f4a0
nicolasdca <nicolasdavid193@gmail.com>
2023-10-25 14:08:40 -0300

    Lógica JS 2 Alura - Aula 3

--- app.js
+++ app.js
@@ -5,26 +5,51 @@
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 let numeroAleatorio = gerarNumeroAleatorio();
+let tentativas = 1;
+let reiniciarBt = document.getElementById('reiniciar');
 
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial() {
+  exibirTextoNaTela('h1', 'Jogo do número secreto');
+  exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
   let chute = document.querySelector('input').value;
   if (chute == numeroAleatorio) {
-    exibirTextoNaTela('p', 'Você acertou!');
+    exibirTextoNaTela('h1', 'Acertou!');
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+    exibirTextoNaTela('p', mensagemTentativas);
+    reiniciarBt.removeAttribute('disabled');
   } else if (chute < numeroAleatorio) {
     exibirTextoNaTela('p', `O número é maior que ${chute}`);
   } else {
     exibirTextoNaTela('p', `O número é menor que ${chute}`);
   }
+  tentativas++;
+  limparChute();
 }
 
 function gerarNumeroAleatorio() {
   return parseInt(Math.random() * 10 + 1);
+}
+
+function limparChute() {
+  chute = document.querySelector('input');
+  chute.value = '';
+}
+
+function reiniciarJogo() {
+  limparChute();
+  reiniciarBt.toggleAttribute('disabled');
+  numeroAleatorio = gerarNumeroAleatorio();
+  exibirMensagemInicial();
+  tentativas = 1;
 }
